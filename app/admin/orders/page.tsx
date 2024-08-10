2c15ab68bbdde4f1a1409a1d1dd1d060cbba0db6
nicole <nicolerosasvallejo@hotmail.com>
2024-08-09 18:22:42 -0600

    tiempo refreshInterval

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -12,7 +12,7 @@ export default function OrdersPage() {
                             .then(data => data);
 
     const {data, error, isLoading} = useSWR<OrderWithProducts []>(url, fetcher, {
-        refreshInterval: 3000
+        refreshInterval: 60000
     });
 
     if(isLoading) return 'Cargando...';
